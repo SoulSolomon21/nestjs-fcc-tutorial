30a047f8016439edf88cd0cc08f4d58b50153e39
Soul_Solomon <sekamattesoul@gmail.com>
2023-04-16 13:08:52 +0300

    created user and bookmark modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
 // this is the main module that will be importing the rest of the other modules
 // A module is any class annotated with the @Module decorator
 // a dedcorator is a function that adds metadata or more properties to a class
+// modules help to organise your code
