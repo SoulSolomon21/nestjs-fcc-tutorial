0a4df683da93dbf1a62569e52a67eb167af7e983
Soul_Solomon <sekamattesoul@gmail.com>
2023-04-16 12:52:03 +0300

    cleaning up files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
+// this is the main module that will be importing the rest of the other modules
+// A module is any class annotated with the @Module decorator
+// a dedcorator is a function that adds metadata or more properties to a class
