1c0870a0d1dbc45a7c30da097ef5098772f65c2a
Soul_Solomon <sekamattesoul@gmail.com>
2023-04-28 13:25:06 +0300

    finished jwt implementation

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,1 +1,17 @@
-export class JwtStrategy {}
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
+// this class is used for validating the token got from the client
+// it is also a provider like the auth service and thus it can use
+// dependency injection which allow us to access the environment variables using the ConfigService
